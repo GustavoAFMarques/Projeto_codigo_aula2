8e8fd5d4a9e8e1e84c6e10d00a9d27110eaf3867
GustavoAFMarques <112992076+GustavoAFMarques@users.noreply.github.com>
2026-03-18 20:05:34 -0300

    Update PyInvest.py

--- Projeto1/PyInvest.py
+++ Projeto1/PyInvest.py
@@ -36,62 +36,61 @@ lucro_lci = montante_lci - total_investido
 montante_lci_liquido = total_investido + (lucro_lci * 0.05)
 
 #poupança
-taxa_poupanca = 0.05
+taxa_poupanca = 0.005
 montante_poupanca = (Capital_inicial * m.pow((1+taxa_poupanca), Prazo_investimento))+(Aporte_mensal * Prazo_investimento)
 
-#FII
-simulacoes_fii = []
+# FII 
+
 taxa_fii_mensal = Rentabilidade_esperada_mensal
 n = int(Prazo_investimento)
 
-for _ in range(5):
-    if taxa_fii_mensal != 0:
-        fator = m.pow(1 + taxa_fii_mensal, n)
-        montante_base = (Capital_inicial * fator) + (Aporte_mensal * ((fator - 1) / taxa_fii_mensal))
-    else:
-        
-        montante_base = Capital_inicial + (Aporte_mensal * n)
+taxa_segura = taxa_fii_mensal + 1e-9
 
-  
-    variacao_final = r.uniform(-0.03, 0.03)
-    montante_ajustado = montante_base * (1 + variacao_final)
+fator = m.pow(1 + taxa_fii_mensal, n)
 
-    simulacoes_fii.append(montante_ajustado)
+montante_base = (Capital_inicial * fator) + (
+    Aporte_mensal * ((fator - 1) / taxa_segura)
+)
 
-# Estatísticas
-media_fii   = s.mean(simulacoes_fii)
-mediana_fii = s.median(simulacoes_fii)
-desvio_fii  = s.stdev(simulacoes_fii) if len(simulacoes_fii) > 1 else 0.0
-montante_fii_oficial = media_fii
-#formatação monetária
+# variação única
+variacao_final = r.uniform(-0.03, 0.03)
+montante_fii_oficial = montante_base * (1 + variacao_final)
+
+# estatísticas
+media_fii   = montante_fii_oficial
+mediana_fii = montante_fii_oficial
+desvio_fii  = 0.0
+
+
+# formatação monetária (sem função)
 l.setlocale(l.LC_ALL, 'pt_BR.UTF-8')
 
-def moeda(valor):
-    return l.currency(valor, grouping=True)
+montante_cdb_fmt = l.currency(montante_cdb_liquido, grouping=True)
+montante_lci_fmt = l.currency(montante_lci_liquido, grouping=True)
+montante_poupanca_fmt = l.currency(montante_poupanca, grouping=True)
+montante_fii_fmt = l.currency(montante_fii_oficial, grouping=True)
 
-#Data de simulação e resgate
 
+# Data de simulação e resgate
 data_simulacao = d.datetime.now()
 data_resgate = data_simulacao + d.timedelta(days=Prazo_investimento * 30)
 
 
-
-atingiu_meta = False
-
-if montante_cdb_liquido >= Meta:
-    atingiu_meta = True
-elif montante_lci_liquido >= Meta:
-    atingiu_meta = True
-elif montante_poupanca >= Meta:
-    atingiu_meta = True
-elif montante_fii_oficial >= Meta:
-    atingiu_meta = True
-
-#ASCII
+# Meta 
+atingiu_meta = (
+    (montante_cdb_liquido >= Meta) or
+    (montante_lci_liquido >= Meta) or
+    (montante_poupanca >= Meta) or
+    (montante_fii_oficial >= Meta)
+)
 
 
-def grafico_ascii(valor):
-    return '█' * int(valor // 1000)
+# ASCII (sem função)
+max_valor = max(montante_cdb_liquido, montante_lci_liquido, montante_poupanca, montante_fii_oficial)
+grafico_cdb = '█' * int((montante_cdb_liquido / max_valor) * 50)
+grafico_lci = '█' * int((montante_lci_liquido / max_valor) * 50)
+grafico_poupanca = '█' * int((montante_poupanca / max_valor) * 50)
+grafico_fii = '█' * int((montante_fii_oficial / max_valor) * 50)
 
 
 
@@ -107,39 +106,34 @@ print("Data da simulação:", data_simulacao.strftime("%d/%m/%Y"))
 print("Data estimada de resgate:", data_resgate.strftime("%d/%m/%Y"))
 print("="*60 + "\n")
 
-print("Total investido:", moeda(total_investido))
+print("Total investido:", total_investido)
 print("\n" + "-"*60)
 print("      RESULTADOS FINANCEIROS")
 print("-"*60 + "\n")
 
 # CDB
-print("CDB:", moeda(montante_cdb_liquido))
-print(grafico_ascii(montante_cdb_liquido) + "\n")
+print("CDB:", montante_cdb_fmt)
+print(grafico_cdb + "\n")
 
 # LCI/LCA
-print("LCI/LCA:", moeda(montante_lci_liquido))
-print(grafico_ascii(montante_lci_liquido) + "\n")
+print("LCI/LCA:", montante_lci_fmt)
+print(grafico_lci + "\n")
 
 # Poupança
-print("Poupança:", moeda(montante_poupanca))
-print(grafico_ascii(montante_poupanca) + "\n")
+print("Poupança:", montante_poupanca_fmt)
+print(grafico_poupanca + "\n")
 
 # FII (média oficial)
-print("FII (média):", moeda(montante_fii_oficial))
-print(grafico_ascii(montante_fii_oficial) + "\n")
+print("FII (média):", montante_fii_fmt)
+print(grafico_fii + "\n")
 
 print("--- ESTATÍSTICAS FII ---")
-print("Média:", moeda(montante_fii_oficial))
-print("Mediana:", moeda(mediana_fii))
-print("Desvio padrão:", moeda(desvio_fii))
+print("Média:", montante_fii_fmt)
+print("Mediana:", montante_fii_fmt)
+print("Desvio padrão:", l.currency(desvio_fii, grouping=True))
 
 print("\nMeta atingida:", atingiu_meta)
 print("_"*60 + "\n")
-
-
-
-
-
 
 
 
